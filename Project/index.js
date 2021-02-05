01752b5bf7e6c77325e174ec408419aab687d4b2
Yuri Khrunyk <62480145+yurikhrunyk@users.noreply.github.com>
2021-02-05 17:14:57 +0200

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,447 @@
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus");
+    const btnMinus = document.querySelector(".btnClassStyleMinus");
+    const pricePerDay = document.querySelector(".prcPerDay").innerHTML;
+    const daysValue = document.querySelector(".calculator").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator").innerHTML = n;
+        document.querySelector(".calculatorTotal").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator").innerHTML = n;
+            document.querySelector(".calculatorTotal").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus1");
+    const btnMinus = document.querySelector(".btnClassStyleMinus1");
+    const pricePerDay = document.querySelector(".prcPerDay1").innerHTML;
+    const daysValue = document.querySelector(".calculator1").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal1").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator1").innerHTML = n;
+        document.querySelector(".calculatorTotal1").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator1").innerHTML = n;
+            document.querySelector(".calculatorTotal1").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus2");
+    const btnMinus = document.querySelector(".btnClassStyleMinus2");
+    const pricePerDay = document.querySelector(".prcPerDay2").innerHTML;
+    const daysValue = document.querySelector(".calculator2").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal2").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator2").innerHTML = n;
+        document.querySelector(".calculatorTotal2").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator2").innerHTML = n;
+            document.querySelector(".calculatorTotal2").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus3");
+    const btnMinus = document.querySelector(".btnClassStyleMinus3");
+    const pricePerDay = document.querySelector(".prcPerDay3").innerHTML;
+    const daysValue = document.querySelector(".calculator3").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal3").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator3").innerHTML = n;
+        document.querySelector(".calculatorTotal3").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator3").innerHTML = n;
+            document.querySelector(".calculatorTotal3").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus4");
+    const btnMinus = document.querySelector(".btnClassStyleMinus4");
+    const pricePerDay = document.querySelector(".prcPerDay4").innerHTML;
+    const daysValue = document.querySelector(".calculator4").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal4").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator4").innerHTML = n;
+        document.querySelector(".calculatorTotal4").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator4").innerHTML = n;
+            document.querySelector(".calculatorTotal4").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus5");
+    const btnMinus = document.querySelector(".btnClassStyleMinus5");
+    const pricePerDay = document.querySelector(".prcPerDay5").innerHTML;
+    const daysValue = document.querySelector(".calculator5").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal5").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator5").innerHTML = n;
+        document.querySelector(".calculatorTotal5").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator5").innerHTML = n;
+            document.querySelector(".calculatorTotal5").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus6");
+    const btnMinus = document.querySelector(".btnClassStyleMinus6");
+    const pricePerDay = document.querySelector(".prcPerDay6").innerHTML;
+    const daysValue = document.querySelector(".calculator6").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal6").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator6").innerHTML = n;
+        document.querySelector(".calculatorTotal6").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator6").innerHTML = n;
+            document.querySelector(".calculatorTotal6").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus7");
+    const btnMinus = document.querySelector(".btnClassStyleMinus7");
+    const pricePerDay = document.querySelector(".prcPerDay7").innerHTML;
+    const daysValue = document.querySelector(".calculator7").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal7").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator7").innerHTML = n;
+        document.querySelector(".calculatorTotal7").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator7").innerHTML = n;
+            document.querySelector(".calculatorTotal7").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus8");
+    const btnMinus = document.querySelector(".btnClassStyleMinus8");
+    const pricePerDay = document.querySelector(".prcPerDay8").innerHTML;
+    const daysValue = document.querySelector(".calculator8").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal8").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator8").innerHTML = n;
+        document.querySelector(".calculatorTotal8").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator8").innerHTML = n;
+            document.querySelector(".calculatorTotal8").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus9");
+    const btnMinus = document.querySelector(".btnClassStyleMinus9");
+    const pricePerDay = document.querySelector(".prcPerDay9").innerHTML;
+    const daysValue = document.querySelector(".calculator9").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal9").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator9").innerHTML = n;
+        document.querySelector(".calculatorTotal9").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator9").innerHTML = n;
+            document.querySelector(".calculatorTotal9").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus10");
+    const btnMinus = document.querySelector(".btnClassStyleMinus10");
+    const pricePerDay = document.querySelector(".prcPerDay10").innerHTML;
+    const daysValue = document.querySelector(".calculator10").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal10").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator10").innerHTML = n;
+        document.querySelector(".calculatorTotal10").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator10").innerHTML = n;
+            document.querySelector(".calculatorTotal10").innerHTML = b;
+        }
+
+    }
+}
+{
+    const btnPlus = document.querySelector(".btnClassStylePlus11");
+    const btnMinus = document.querySelector(".btnClassStyleMinus11");
+    const pricePerDay = document.querySelector(".prcPerDay11").innerHTML;
+    const daysValue = document.querySelector(".calculator11").innerHTML;
+    const pricePerDayTotal = document.querySelector(".calculatorTotal11").innerHTML;
+
+    let daysValueNum = parseInt(daysValue, Number);
+    let pricePerDayNum = parseInt(pricePerDay, Number);
+    let pricePerDayNumStat = pricePerDayNum;
+    let pricePerDayTotalNum = parseInt(pricePerDayTotal, Number);
+    console.log(pricePerDay);
+
+    btnPlus.onclick = () => {
+        daysValueNum = daysValueNum + 1;
+        let n = daysValueNum.toString();
+        pricePerDayTotalNum = pricePerDayTotalNum + pricePerDayNumStat;
+        let b = pricePerDayTotalNum.toString();
+        document.querySelector(".calculator11").innerHTML = n;
+        document.querySelector(".calculatorTotal11").innerHTML = b;
+    }
+    console.log(pricePerDayTotal);
+    btnMinus.onclick = () => {
+        if (pricePerDayTotalNum > 0) {
+            daysValueNum = daysValueNum - 1;
+            let n = daysValueNum.toString();
+            pricePerDayTotalNum = pricePerDayTotalNum - pricePerDayNumStat;
+            let b = pricePerDayTotalNum.toString();
+            document.querySelector(".calculator11").innerHTML = n;
+            document.querySelector(".calculatorTotal11").innerHTML = b;
+        }
+
+    }
+}
+
+{
+    const btn = document.querySelector(".submitButton");
+    const nameInput = document.querySelector(".nameInput");
+    const textArea = document.querySelector(".commTextArea");
+    const intupDiv = document.querySelector(".intupDiv");
+    const divWithComm = document.querySelector(".divWithComm");
+
+    btn.onclick = () => {
+        if (nameInput.value === "" || textArea.value === "") {
+        } else {
+            const nameValue = nameInput.value;
+            const textValeu = textArea.value;
+            nameInput.value = '';
+            textArea.value = '';
+            let wrapper = document.createElement('div');
+            wrapper.classList.add("commentOne");
+            divWithComm.appendChild(wrapper);
+            let imgAv = document.createElement("IMG");
+            imgAv.classList.add("userPick");
+            imgAv.setAttribute("src", './img/user.png')
+            wrapper.appendChild(imgAv);
+            let userNiqName = document.createElement("h5");
+            userNiqName.classList.add("userNiq");
+            userNiqName.innerHTML = nameValue;
+            wrapper.appendChild(userNiqName);
+            let userTextComment = document.createElement("p");
+            userTextComment.classList.add("userComm");
+            userTextComment.innerHTML = textValeu;
+            wrapper.appendChild(userTextComment);
+            textArea.addEventListener("keyup", function (event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault();
+                    document.querySelector(".submitButton").click();
+                }
+            });
+        }
+    }
+}
